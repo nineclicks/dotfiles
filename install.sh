90c6dec92bec6555b6d7e9856eef3e9c37f4270b
Nick <nicholasg1989@gmail.com>
2023-01-05 11:32:11 -0800

    fix tmux, tpm

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 git submodule update --init --recursive
 ln -sfn $PWD/bash_aliases ~/.bash_aliases
 ln -sfn $PWD/tmux.conf    ~/.tmux.conf
+ln -sfn $PWD/tmux         ~/.tmux
 ln -sfn $PWD/vim          ~/.vim
 ln -sfn $PWD/vimrc        ~/.vimrc
 ln -sfn $PWD/xmodmap      ~/.xmodmap
+
+$PWD/tmux/plugins/tpm/bin/install_plugins
 
 if [ "$(uname)" == "Darwin" ]; then
   # if Mac
